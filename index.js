00c38acc0db7ebb5bb264448ef882296c13e9512
thotamanikanta23 <46632247+thotamanikanta23@users.noreply.github.com>
2025-02-11 13:56:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const core = require("@actions/core");
 try {
   const name = core.getInput("name");
   const message = `Hello, ${name}! Welcome to GitHub Actions.`;
-  core.setOutput("message", message);
+  core.setOutput("greeting", message);
 } catch (error) {
   core.setFailed(error.message);
 }
